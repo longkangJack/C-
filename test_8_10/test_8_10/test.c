975bc3ff9d6f9c953a32e995ea372a16ade0d31e
longkangjack <1030269471@qq.com>
2019-08-10 15:57:58 +0800

    8-10

---
+++ test_8_10/test_8_10/test.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int n = 0;
+//	printf("ÇëÊäÈë²Ù×÷Êı\n");
+//	scanf_s("%d\n", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%d ", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 101; i <200; i+=2)
+//	{
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i%j == 0)
+//				break;
+//		}
+//		if (j == i)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+//			printf("%d ", year);
+//	}
+//	system("pause");
+//	return 0;
+//}

e15e5dee4f693b0b76b72afaf49dd4e1977dfbcb
longkangjack <1030269471@qq.com>
2019-09-01 22:48:26 +0800

    9-1

---
+++ test_9_1/test_9_1/Contact.h
@@ -0,0 +1,90 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+#include<string.h>
+
+#include<assert.h>
+
+//函数的声明和头文件的书写
+
+//10000个人信息包括 名字 性别 年龄 地址 电话
+
+//增加
+
+//删除
+
+//查找
+
+//修改
+
+//显示
+
+//排序
+
+#define MAX 1000
+
+#define NAME_MAX 20
+
+#define TELE_MAX 12
+
+#define ADDR_MAX 25
+
+#define SEX_MAX 5
+
+enum Option//枚举法 列举出通讯录的功能
+
+{
+
+	EXIT,//退出
+
+	Add,//增加
+
+	Del,//删除
+
+	search,//查找
+
+	modif,
+
+	show,//展示通讯录
+
+	bubble,//排序
+	//枚举法
+
+};
+
+//人的信息
+
+typedef struct PeoInfo
+
+{
+
+	char name[NAME_MAX];
+
+	char sex[SEX_MAX];
+
+	short age;
+
+	char addr[ADDR_MAX];
+
+	char tele[TELE_MAX];
+
+}PeoInfo;
+
+//通讯录结构体
+
+typedef struct Contact
+
+{
+
+	PeoInfo data[1000];
+
+	int sz;
+
+}Contact;
+
+void InitContact(Contact* pcon);
+void AddContact(Contact* pcon);
+void ShowContact(Contact* pcon);
+void DelContact(Contact* pcon);
+void SearchContact(Contact* pcon);
---
+++ test_9_1/test_9_1/contact.c
@@ -0,0 +1,163 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"contact.h"
+
+//函数的实现
+
+void InitContact(Contact* pcon)//初始化通讯录
+
+{
+
+	pcon->sz = 0;
+
+	memset(pcon->data, 0, sizeof(pcon->data));//名称 初始化成什么，大小
+
+}
+
+void AddContact(Contact* pcon)
+
+{
+
+	if (pcon->sz == MAX)
+
+	{
+
+		printf("通讯录已满,无法添加\n");
+
+		return;
+
+	}
+
+	printf("请输入名字:>");
+
+	scanf("%s", pcon->data[pcon->sz].name);
+
+	printf("请输入年龄:>");
+
+	scanf("%d", &(pcon->data[pcon->sz].age));
+
+	printf("请输入性别:>");
+
+	scanf("%s", pcon->data[pcon->sz].sex);
+
+	printf("请输入地址:>");
+
+	scanf("%s", pcon->data[pcon->sz].addr);
+
+	pcon->sz++;
+
+	printf("增加成功\n");
+
+}
+
+void ShowContact(const Contact* pcon)
+
+{
+
+	int i = 0;
+
+	assert(pcon);
+
+	printf("%15s\t%5s\t%5s\t%12s\t%20s\n", "名字", "年龄", "性别", "电话", "地址");
+
+	for (i = 0; i < pcon->sz; i++)
+
+	{
+
+		printf("%15s\t%5d\t%5s\t%12s\t%20s\n", pcon->data[i].name, pcon->data[i].age, pcon->data[i].sex, pcon->data[i].tele, pcon->data[i].addr);
+
+	}
+}
+static FindName(Contact* pcon, char name[])
+
+{
+
+	int i = 0;
+
+	assert(pcon);
+
+	for (i = 0; i < pcon->sz; i++)
+
+	{
+
+		if (0 == strcmp(pcon->data[i].name, name))
+
+		{
+
+			return i;
+
+		}
+
+	}
+
+	return -1;
+
+}
+
+void DelContact(Contact* pcon)
+
+{
+
+	int i = 0;
+
+	char name[NAME_MAX] = { 0 };
+
+	int pos = 0;
+
+	assert(pcon);
+
+
+
+	if (pcon->sz == 0)
+
+	{
+
+		printf("通讯录为空,无法删除\n");
+
+		return;
+
+	}
+
+	printf("请输入要删除人的名字\n");
+
+	scanf("%s", name);
+
+	//查找
+
+	pos = FindName(pcon, name);
+
+	if (-1 == pos)
+
+	{
+
+		printf("要删除的人不存在\n");
+
+		return;
+
+	}
+	for (i = 0; i < pcon->sz; i++)
+	{
+		pcon->data[i] = pcon->data[i + 1];
+	}
+	pcon->sz--;
+	printf("删除成功\n");
+}
+void SearchContact(Contact* pcon)
+{
+	char name[NAME_MAX] = { 0 };
+	int pos = 0;
+	assert(pcon);
+	printf("请输入要查找的姓名>");
+	scanf("%s", name);
+	pos = FindName(pcon, name);
+	if (-1 == pos)
+	{
+		printf("要查找的人不存在:>");
+	}
+	else
+	{
+		printf("%15s\t%5d\t%5s\t%15s\t%20s\n", "姓名", "年龄", "性别", "电话", "住址");
+		printf("%15s\t%5d\t%5s\t%15s\t%20s\n",pcon->data[pos].name,pcon->data[pos].age,pcon->data[pos].)
+	}
+
+}

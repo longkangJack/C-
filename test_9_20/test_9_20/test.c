bc6b44ec83c36c5273458d66c138f43a9f6a8161
longkangjack <1030269471@qq.com>
2019-09-21 23:30:39 +0800

    9-20

---
+++ test_9_20/test_9_20/test.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	printf("请输入两个操作数");
+//	scanf_s("%d %d", &a, &b);
+//	if (a > b)
+//	{
+//		printf("you are stupid\n");
+//	}
+//	else
+//	{
+//		printf("you are beautiful\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int a = 0;
+//	i = ((a = 2 * 3, a * 5), a + 6);
+//	printf("%d", i);
+//	return 0;
+//}
+int main()
+{
+	printf("Hello World\n");
+	return 0;
+}

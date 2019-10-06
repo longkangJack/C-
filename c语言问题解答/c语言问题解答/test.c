420b1c0b79047ee7914e11ad6ffdec2c0022635c
longkangjack <1030269471@qq.com>
2019-10-06 13:49:26 +0800

    c语言习题

---
+++ c语言问题解答/c语言问题解答/test.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 1;
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		j = j*i;
+//	}
+//	printf("%d\n", j);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	char c = 0;
+//	scanf_s("=<", &a, &c);
+//	printf("%d,%c", a, c);
+//	return 0;
+//}
+//int main()
+//{
+//	int fahr = 100;
+//	int celsius = 0;
+//	celsius = 5 * (fahr - 32) / 9;
+//	printf("fahr = 100, celsius = %d\n", celsius);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int t = 0;
+//	printf("请输入要比较的三个数字>");
+//	scanf_s("%d %d %d", &a, &b, &c);
+//	if (a > b)
+//	{
+//		t = a;
+//		a = b;
+//		b = t;
+//	}
+//	if (a > c)
+//	{
+//		t = a;
+//		a = c;
+//		c = t;
+//	}
+//	if (b>c)
+//	{
+//		t = b;
+//		b = c;
+//		c = t;
+//	}
+//	printf("%d %d %d\n", a, b, c);
+//	return 0;
+//}
+//typedef struct student
+//{
+//	char name[10];
+//	int classA;
+//	float ShuXue;
+//	float YingYu;
+//	float YuWen;
+//}stu;
+//int main()
+//{
+//	stu s;
+//	int i = 0;
+//	for (i = 0; i < 2; i++)
+//	{
+//		printf("请输入第一个同学的信息>\n");
+//		scanf_s("%s %d %f %f %f",s.name,&s.classA,&s.ShuXue,&s.YingYu,&s.YuWen);
+//		printf("%s %d %f %f %f\n", s.name, s.classA, s.ShuXue, s.YingYu, s.YuWen);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch <= 'Z'&&ch >= 'A'&&ch != '|')
+//		{
+//			putchar(ch + 32);
+//		}
+//		
+//	}
+//	return 0;
+//}
+
+
+	
+	
